8c5ceb38b5c94e72e54e65cb820bbd03fff3e1bd
Kotaro0722 <kotaro.tanaka@4nonome.com>
2024-11-20 00:35:24 +0900

    フッター修正

--- src/page/footer/index.tsx
+++ src/page/footer/index.tsx
@@ -1,4 +1,16 @@
-import { Fab, useScrollTrigger, Zoom } from "@mui/material";
+import {
+  AppBar,
+  Button,
+  Fab,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+  useScrollTrigger,
+  Zoom,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
@@ -11,22 +23,66 @@ export const Footer = () => {
   const handleClick = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
+  const handleOnGitHubClick = () => {
+    window.location.href = "https://github.com/Kotaro0722";
+  };
+
   return (
-    <Zoom in={trigger}>
-      <Box
-        onClick={handleClick}
-        role="presentation"
-        sx={{
-          position: "fixed",
-          bottom: 16,
-          right: 16,
-          zIndex: 1000,
-        }}
+    <>
+      <AppBar
+        position="static"
+        enableColorOnDark
+        sx={{ backgroundColor: "white" }}
       >
-        <Fab color="default" size="small" aria-label="scroll back to top">
-          <KeyboardArrowUpIcon />
-        </Fab>
-      </Box>
-    </Zoom>
+        <Toolbar
+          sx={{
+            height: "50px",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              flexGrow: 1,
+              gap: "30px",
+            }}
+          >
+            <Typography
+              sx={{
+                color: "black",
+                lineHeight: "64px",
+                textAlign: "center",
+              }}
+            >
+              ©2024 Kotaro Tanaka
+            </Typography>
+            <Button onClick={handleOnGitHubClick}>
+              <img
+                src="img/github.svg"
+                alt="github"
+                style={{ width: "30px" }}
+              />
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Zoom in={trigger}>
+        <Box
+          onClick={handleClick}
+          role="presentation"
+          sx={{
+            position: "fixed",
+            bottom: 16,
+            right: 16,
+            zIndex: 1000,
+          }}
+        >
+          <Fab color="default" size="small" aria-label="scroll back to top">
+            <KeyboardArrowUpIcon />
+          </Fab>
+        </Box>
+      </Zoom>
+    </>
   );
 };
