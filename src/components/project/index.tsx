c61682008e9485acd83accf98217606b465c88a0
Kotaro0722 <kotaro.tanaka@4nonome.com>
2024-11-24 00:11:20 +0900

    画面遷移できるように修正

--- src/components/project/index.tsx
+++ src/components/project/index.tsx
@@ -9,6 +9,7 @@ import {
   Typography,
   Box,
   Divider,
+  CardActionArea,
 } from "@mui/material";
 import { date2YearMonthString } from "../../utils/dateFormatter";
 import GitHubIcon from "@mui/icons-material/GitHub";
@@ -23,11 +24,6 @@ export type ProjectPartsType = {
   skill: Array<string>;
 };
 
-type ProjectContentModalPRops = {
-  isOpen: boolean;
-  handleOnClose: (open: boolean) => void;
-};
-
 export const ProjectParts = ({
   title,
   date,
@@ -37,81 +33,43 @@ export const ProjectParts = ({
   github,
   skill,
 }: ProjectPartsType) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const handleOnClickCard = () => {
-    setIsOpen(true);
-  };
-  const handleOnClose = () => {
-    setIsOpen(false);
-  };
-
   return (
-    <>
-      <Card sx={{ cursor: "pointer" }}>
-        <Box onClick={handleOnClickCard}>
-          <CardHeader title={title} subheader={date2YearMonthString(date)} />
-          <CardMedia component="img" height="300" image={img} />
-          <CardContent>
-            <Typography>{abstract}</Typography>
-            <Divider sx={{ my: 3 }} />
-            <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
-              <Typography> 使用技術</Typography>
-              <Box sx={{ display: "flex", gap: 1 }}>
-                {skill.map((sk, index) => (
-                  <img
-                    src={"img/" + sk + ".svg"}
-                    alt=""
-                    key={index}
-                    width="40px"
-                  />
-                ))}
-              </Box>
+    <Card>
+      <Box>
+        <CardHeader title={title} subheader={date2YearMonthString(date)} />
+        <CardActionArea href={contentLink} disabled={!contentLink}>
+          <CardMedia
+            component="img"
+            height="300"
+            image={img}
+            sx={{ cursor: contentLink && "pointer" }}
+          />
+        </CardActionArea>
+        <CardContent>
+          <Typography>{abstract}</Typography>
+          <Divider sx={{ my: 3 }} />
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+            <Typography> 使用技術</Typography>
+            <Box sx={{ display: "flex", gap: 1 }}>
+              {skill.map((sk, index) => (
+                <img
+                  src={"img/" + sk + ".svg"}
+                  alt=""
+                  key={index}
+                  width="40px"
+                />
+              ))}
             </Box>
-          </CardContent>
-          {github && (
-            <CardActions>
-              <IconButton aria-label="link to github" href={github}>
-                <GitHubIcon />
-              </IconButton>
-            </CardActions>
-          )}
-        </Box>
-      </Card>
-    </>
+          </Box>
+        </CardContent>
+        {github && (
+          <CardActions>
+            <IconButton aria-label="link to github" href={github}>
+              <GitHubIcon />
+            </IconButton>
+          </CardActions>
+        )}
+      </Box>
+    </Card>
   );
 };
-
-// const ProjectContentModal = ({
-//   contents,
-//   isOpen,
-//   handleOnClose,
-// }: ProjectContentModalPRops) => {
-//   return (
-//     <Modal
-//       open={isOpen}
-//       onClose={handleOnClose}
-//       sx={{
-//         display: "flex",
-//         alignItems: "center",
-//         justifyContent: "center",
-//       }}
-//     >
-//       <Paper
-//         sx={{
-//           width: "70%",
-//           height: "70%",
-//         }}
-//       >
-//         {contents.map((content, index) => {
-//           return (
-//             <DynamicMUIComponent
-//               componentName={content[0] as MUIComponentName}
-//               children={content[1]}
-//               key={index}
-//             />
-//           );
-//         })}
-//       </Paper>
-//     </Modal>
-//   );
-// };
